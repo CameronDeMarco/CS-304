8776f405409cb81f2474d84a4a663bf4d7fc2b31
Kyle S <shawkyle06@yahoo.com>
2024-02-22 10:38:16 -0500

    camera and gallery working
    Able to select images from the camera or gallery. Each image added is added as a list item displaying the image, image filename, and a future delete button.

--- Screens/UploadPage.jsx
+++ Screens/UploadPage.jsx
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, Pressable , FlatList} from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, Pressable , FlatList, Image} from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { RawButton, ScrollView } from 'react-native-gesture-handler';
-import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+import * as ImagePicker from 'expo-image-picker';
 
 
 const UploadPage = () => {
     const theme = useTheme();
     const [ITEMLIST, setItem] = useState([]);
 
-    function addFile(){
-        setItem([...ITEMLIST, {fileName:"test.png"}]);
+    function addFile(fileName, uri){
+        setItem([...ITEMLIST, {fileName:fileName, uri:uri}]);
     }
 
-    function selectMedia(){
-        //TODO implement camera/galery options...
-        //If user adds media call add file.
+    async function openCamera(){
+        let result = await ImagePicker.launchCameraAsync();
+        if(!result.canceled){
+            console.log(result.assets[0].fileName + " selected.");
+            addFile(test=result.assets[0].fileName, result.assets[0].uri);
+            console.log(result.assets[0]);
+        }else{
+            console.log("Media selection cancled.");
+        }
+    }
+
+    async function selectMedia(){
+        let result = await ImagePicker.launchImageLibraryAsync();
+
+        if(!result.canceled){
+            console.log(result.assets[0].fileName + " selected.");
+            addFile(test=result.assets[0].fileName, result.assets[0].uri);
+            console.log(result.assets[0]);
+        }else{
+            console.log("Media selection cancled.");
+        }
     }
 
     return (
@@ -29,17 +47,27 @@ const UploadPage = () => {
                 <Text style={styles.textField}>Description</Text>
                 <TextInput maxLength={150} style={[styles.multiTextInput, styles.androidShadow]} multiline></TextInput>
             </View>
-            <Pressable 
-            onPressOut={()=>{
-                addFile();
-                selectMedia();
-            }}>
-                <Text style={{color:"#3CCDB7", fontSize:19, width: '100%', height: 75, textAlignVertical:'center', textAlign:'center'}}>+ Add Media</Text>
-            </Pressable>
+            <Text style={{fontWeight:"bold", fontSize:22, textDecorationLine:"underline", textAlign:'center', marginTop:20, marginBottom:5}}>Media</Text>
+            <View style={{flexDirection:"row", justifyContent:"center"}}>
+                <Pressable
+                style={{marginHorizontal:15}}
+                onPressOut={()=>{
+                    openCamera();
+                }}>
+                    <Text style={{color:"#3CCDB7", width:"100%", fontSize:19, marginBottom:25, textAlignVertical:'center'}}>+ Camera</Text>
+                </Pressable>
+                <Pressable 
+                style={{marginHorizontal:15}}
+                onPressOut={()=>{
+                    selectMedia();
+                }}>
+                <Text style={{color:"#3CCDB7", width:"100%", fontSize:19, marginBottom:25, textAlignVertical:'center'}}>+ Gallery</Text>
+                </Pressable>
+            </View>
             <FlatList 
                 scrollEnabled={false}
                 data={ITEMLIST}
-                renderItem={({item}) => <ResourceCard fileName={item.fileName}/>}
+                renderItem={({item}) => <ResourceCard fileName={item.fileName} uri={item.uri}/>}
             />
             <View style={{paddingTop:15, width: 100, height: 100}}>
                 <Button title="Upload" disabled />
@@ -83,10 +111,17 @@ const styles = StyleSheet.create({
 
 export default UploadPage;
 
-const ResourceCard = ({fileName}) => {
+const ResourceCard = ({fileName, uri}) => {
+    let resourceName;
+    if(fileName.length > 20){
+        resourceName = fileName.substring(0,20) + "...";
+    }else{
+        resourceName = fileName;
+    }
     return(
-        <View style={{flexDirection:"row", justifyContent:"space-between", flex:1, marginHorizontal: 30, marginVertical:8, backgroundColor:"#E6E6E6", borderWidth:.25}}>
-            <Text style={{padding:15}}>{fileName}</Text>
+        <View style={{flexDirection:"row", justifyContent:"flex-start", flex:1, marginHorizontal: 30, marginVertical:8, backgroundColor:"#E6E6E6"}}>
+            <Image source={{uri:uri}} width={75} height={75}/>
+            <Text style={{padding:15, textAlignVertical:"center"}}>{resourceName}</Text>
             <Pressable>
                 <MaterialCommunityIcons.Button name="image-remove" size={24} color="black" backgroundColor={"#00000000"} style={{height:"100%"}}/>
             </Pressable>
